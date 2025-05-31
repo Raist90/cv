df211a2cc3a21c6278a57f7c2a3e49e23e1b5631
Manuel De Toma <manuel.detoma@gmail.com>
2025-05-31 15:54:52 +0200

    feat: add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import { createConfigForNuxt } from "@nuxt/eslint-config/flat";
 
-export default withNuxt(
+export default createConfigForNuxt({
   // Your custom configs here
-)
+  features: {
+    stylistic: {
+      indent: 2,
+      quotes: "double",
+      semi: true,
+    },
+  },
+})
+  .override("nuxt/vue/rules", {
+    rules: {
+      "vue/attribute-hyphenation": ["error", "never"],
+      "vue/html-self-closing": "off",
+      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  });
