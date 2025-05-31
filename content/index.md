79980fedc52f58d7954e16ac5d1754d7ae8721ee
Manuel De Toma <manuel.detoma@gmail.com>
2025-05-31 15:55:04 +0200

    feat: add nuxt content

---
+++ content/index.md
@@ -0,0 +1,4 @@
+# A title
+## A subtitle
+
+Hello, this is my content. It is written in Markdown.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+  ],
+  devtools: { enabled: false },
+  compatibilityDate: "2025-05-15",
+});
