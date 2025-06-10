32fbca6dec1670a747dd1fdeda3ba182376688f2
Manuel De Toma <manuel.detoma@gmail.com>
2025-06-10 22:46:40 +0200

    feat: make Header sticky

--- components/Header.vue
+++ components/Header.vue
@@ -9,7 +9,7 @@ defineProps<Props>();
 </script>
 
 <template>
-  <nav class="border border-stone-700 py-2 px-4 uppercase font-bold">
+  <nav class="border border-stone-700 py-2 px-4 uppercase font-bold sticky top-2.5 bg-stone-950/80 z-10 backdrop-blur-md">
     <ul class="flex gap-x-8 justify-center">
       <li
         v-for="link in links"
