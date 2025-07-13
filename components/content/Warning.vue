1788e2a946637659fea796c27ab691108551feb6
Manuel De Toma <manuel.detoma@gmail.com>
2025-07-13 22:19:27 +0200

    feat: add Warning component and relevant translations

---
+++ components/content/Warning.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { type Locale, useTranslation } from "~/composables/useTranslation";
+
+const { public: { locale } } = useRuntimeConfig();
+const { t } = await useTranslation(locale as Locale);
+</script>
+
+<template>
+  <div class="border border-yellow-500/50 bg-yellow-500/10 p-4 mb-8 space-y-2">
+    <h4
+      class="font-bold"
+      v-text="t('misc.warning')"
+    />
+
+    <div class="text-sm">
+      <slot />
+    </div>
+  </div>
+</template>
